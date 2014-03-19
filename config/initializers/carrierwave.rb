95558a4662c12bef4065eca19c1bf9232d901e75
kt32291 <Katharine322@gmail.com>
2014-03-19 11:52:32 -0500

    now actually heroku ready

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
     :region                 => 'us-east-1'
   }
-  config.fog_directory  = 'accomplishit-assets'
+  config.fog_directory  = 'stagedirection-assets'
   config.storage = :fog
   # see https://github.com/jnicklas/carrierwave#using-amazon-s3
   # for more optional configuration
